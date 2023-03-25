ff72ff8d664c30c775a61084475efc20e2deb7a1
Fede Colio <federicocolio1@MacBook-Pro-di-Federico.local>
2023-03-25 23:55:52 +0000

    Improved inputs readings as well as options

--- utils.py
+++ utils.py
@@ -3,13 +3,18 @@ from typing import Dict, List, Tuple
 import pyjokes
 
 
-def create_dict(file_path: str) -> Tuple[Dict[str, str], List[str]]:
+def create_dict(
+    file_path: str, from_lan: str, to_lan: str, delimiter: str = ","
+) -> Tuple[Dict[str, str], List[str]]:
     """
     Reads a CSV file with two columns and returns a dictionary
     containing the key-value pairs, and a list of the keys.
 
     Args:
         file_path (str): The path to the CSV file to read.
+        from_lan (str): The name of the column containing the keys in the CSV file.
+        to_lan (str): The name of the column containing the values in the CSV file.
+        delimiter (str): The delimiter used in the CSV file. Defaults to ','.
 
     Returns:
         A tuple containing:
@@ -26,10 +31,10 @@ def create_dict(file_path: str) -> Tuple[Dict[str, str], List[str]]:
     try:
         # Open the CSV file and create a dictionary reader
         with open(file_path, newline="") as csvfile:
-            reader = csv.DictReader(csvfile, delimiter=",")
+            reader = csv.DictReader(csvfile, delimiter=delimiter)
 
             # Use a dict comprehension to create the dictionary from the CSV data
-            data_dict = {rows["ITALIAN"]: rows["ENGLISH"] for rows in reader}
+            data_dict = {rows[from_lan]: rows[to_lan] for rows in reader}
             # Get the list of keys from the dictionary
             keys_list = list(data_dict.keys())
 
